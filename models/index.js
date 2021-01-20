abb467d5e3572598786f3411eb93fe7d3813bc47
신 진섭 <yeo11200@hanmail.net>
2021-01-20 08:00:22 -0800

    testt

--- models/index.js
+++ models/index.js
@@ -5,6 +5,9 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
+
+console.log(process.env.NODE_ENV);
+
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
