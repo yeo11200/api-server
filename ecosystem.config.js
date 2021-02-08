c895bbe0a0a4f3ef46a9311d0a86175082a2ad06
shinjinseop <yeo11200@hanmail.net>
2021-02-09 00:22:21 +0900

    jenkins test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: "App name", // pm2로 실행한 프로세스 목록에서 이 애플리케이션의 이름으로 지정될 문자열
-            script: "./bin/www", // pm2로 실행될 파일 경로
+            script: "./app.js", // pm2로 실행될 파일 경로
             watch: true, // 파일이 변경되면 자동으로 재실행 (true || false)
             env: {
                 "NODE_ENV": "development" // 개발환경시 적용될 설정 지정
